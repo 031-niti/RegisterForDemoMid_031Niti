31f5e059fccb38ee6b099e3597250a583d9fbfc7
Niti2003ss <earthza6263@gmail.com>
2023-02-10 15:34:02 +0700

    Initial commit

---
+++ Scripts/TC1001RegisterForDemo/Script1676013562275.groovy
@@ -0,0 +1,75 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://nxtgenaiacademy.com/demo-site/')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input__vfb-5'), 'Niti')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input__vfb-7'), 'Surakongka')
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Gender_vfb-31'))
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Address_vfb-13address'), '147/1')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Street Address_vfb-13address-2'), 
+    'แสงชูโตใหม่')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Apt, Suite, Bldg. (optional)_vfb-13city'), 
+    'ท่ามะกา')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_City_vfb-13state'), 'กาญจนบุรี')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_State  Province  Region_vfb-13zip'), 
+    '71130')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input__vfb-14'), '644259031@webmail.npru.ac.th')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Date of Demo_vfb-18'), '02/10/2023')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Mobile Number_vfb-19'), '0917582874')
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Course Interested_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Selenium WebDriver_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_UFT_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_TestNG_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Core Java_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_Functional Testing_vfb-20'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/textarea_Enter your query_vfb-23'), 
+    'Mid_Niti031')
+
+WebUI.setText(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input__vfb-3'), '99')
+
+WebUI.click(findTestObject('Page_Demo Site  Registration Form  NxtGen A.I Academy/input_This box is for spam protection - strongplease leave it blankstrong_vfb-submit'))
+
+WebUI.verifyTextPresent('Dynamic Transaction Verification', false)
+
+WebUI.takeScreenshot()
+
+WebUI.delay(2)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/TS1001.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1742b96e-948a-4d85-8db3-f11b3dcbd9e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>a77d3d62-3bb1-4cc6-a460-55976a2a1413</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1001RegisterForDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73865f0a-ee3d-490d-89ca-e3b2c595beac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1002RegisterForDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
